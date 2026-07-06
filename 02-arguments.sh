d0f6cce60e1d51d1597c72147474e1f6b49def04
ramcharanpalagit config --global user.mail ramcharanpala1257@gmail.com <ramcharanpala1257@gmail.comgit config --global user.name ramcharanpala>
2026-07-06 19:11:34 +0530

    hello world

---
+++ 02-arguments.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#arguments
+$PERSON1="$1"
+$PERSON2="$2"
+
+echo "$PERSON1:: hi how are you $PERSON2"
+echo "$PERSON2:: i am fine $PERSON1,what about you"
+echo "$PERSON1:: i am doing well, thanks for asking $PERSON2"
+echo "$PERSON2:: hope good to hear $PERSON1"
+echo "$PERSON1:: where are now $PERSON2"
+echo "$PERSON2:: i am in balepalli $PERSON1"
