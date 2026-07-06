5bb628cc8760aedea2c0dbea39d4372ce066b807
ramcharanpalagit config --global user.mail ramcharanpala1257@gmail.com <ramcharanpala1257@gmail.comgit config --global user.name ramcharanpala>
2026-07-06 22:01:43 +0530

    variables

---
+++ 07-specialvariables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "all variables passed to the script: $@"
+echo "all variables passed to the script: $*"
+echo "script name: $0"
+echo "current directory: $PWD"
+echo "who is running the script: $USER"
+echo "home directory of the user: $HOME"
+echo "PID of the script: $$"
