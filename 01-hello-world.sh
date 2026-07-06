d704583c61f8b6e992b1e96d50ae9879b6317bf9
ramcharanpalagit config --global user.mail ramcharanpala1257@gmail.com <ramcharanpala1257@gmail.comgit config --global user.name ramcharanpala>
2026-07-06 18:47:57 +0530

    hello world

---
+++ 01-hello-world.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1="ram"
+PERSON2="pram"
+
+echo $PERSON1:: hi how are you $PERSON2
+echo $PERSON2:: i am fine $PERSON1,what about you
+echo $PERSON1:: i am doing well, thanks for asking $PERSON2
+echo $PERSON2:: that's good to hear $PERSON1
