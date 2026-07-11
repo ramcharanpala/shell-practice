d1619aa53d1a395c12eba464f53c366910b06278
ramcharanpalagit config --global user.mail ramcharanpala1257@gmail.com <ramcharanpala1257@gmail.comgit config --global user.name ramcharanpala>
2026-07-11 17:21:31 +0530

    conditions

---
+++ 09-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+NUMBER=$
+
+if [ $NUMBER -gt 10 ]; then
+    echo "The number is greater than 10."
+elif [ $NUMBER -eq 10 ]; then
+    echo "The number is equal to 10."
+else
+    echo "The number is less than 10."
+fi
