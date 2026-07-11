6050fc72923def3ca7e3984b4cd2ca9f3b9f9605
ramcharanpalagit config --global user.mail ramcharanpala1257@gmail.com <ramcharanpala1257@gmail.comgit config --global user.name ramcharanpala>
2026-07-11 22:18:56 +0530

    conditions

--- 14-logs.sh
+++ 14-logs.sh
@@ -3,10 +3,10 @@
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
-y="\e[33m"
+Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shell-scipt"
+LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
